233ad80b98c25890be000abf5ca43f0c80875dd2
Cesar Aybar <csaybar@gmail.com>
2021-11-01 01:33:33 +0100

    Add files via upload

--- script05.R
+++ script05.R
@@ -1,23 +1,40 @@
+#' Section 04: Display map interactively.
+#' @author Antony Barja, Cesar Aybar
+
 library(rgee)
 library(rgeeExtra)
 
 ee_Initialize()
 
-# Load a Javascript module
+# ------------------------------------------------------------------------------
+# 1. Load a Javascript module
+# ------------------------------------------------------------------------------
 LandsatLST <- module("users/sofiaermida/landsat_smw_lst:modules/Landsat_LST.js")
 
-# Define Landsat_LST parameters
+# ------------------------------------------------------------------------------
+# 2. Define Landsat_LST parameters
+# ------------------------------------------------------------------------------
 geometry <- ee$Geometry$Rectangle(c(-8.91, 40.0, -8.3, 40.4))
 satellite <- 'L8'
 date_start <- '2018-05-15'
 date_end <- '2018-05-31'
 use_ndvi <- TRUE
 
-# Run the model
-LandsatColl <- LandsatLST$collection(satellite, date_start, date_end, geometry, use_ndvi)
+# ------------------------------------------------------------------------------
+# 3. Run the model
+# ------------------------------------------------------------------------------
+LandsatColl <- LandsatLST$collection(
+  landsat = satellite, 
+  date_start = date_start, 
+  date_end = date_end, 
+  geometry = geometry, 
+  use_ndvi = use_ndvi
+)
 exImage <- LandsatColl$first()
 
-# Create an interactive map
+# ------------------------------------------------------------------------------
+# 4. Create an interactive map
+# ------------------------------------------------------------------------------
 cmap <- c('blue', 'cyan', 'green', 'yellow', 'red')
 lmod <- list(min = 290, max = 320, palette = cmap)
 Map$centerObject(geometry)
