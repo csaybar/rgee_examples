8801b5cd430e64fe018f4c76c44bdf9e82add5c2
Cesar Aybar <csaybar@gmail.com>
2022-02-26 07:03:15 +0100

    Update script01.R

--- script01.R
+++ script01.R
@@ -1,5 +1,6 @@
 #' Section 01: Installing rgee
-#' @author Angie Flores
+#' This script has all the essentials to install all the rgee R and Python dependencies.
+#' @author Angie Flores and Cesar Aybar
 
 # --------------------------------------------------------------
 # 1. Install rgee R dependencies
@@ -9,22 +10,40 @@
 install.packages("rgee")
 
 # --------------------------------------------------------------
-# 2. Install rgee Python dependencies
-# --------------------------------------------------------------
-rgee::ee_install() # Window user must need to terminate R and enter to R again.
-
-# --------------------------------------------------------------
-# 3. Install a specific Earth Engine Python API version.
-# --------------------------------------------------------------
-ee_install_upgrade(version = "0.1.232")
-
-# --------------------------------------------------------------
-# 4. Load rgee and authenticate your EE acount
+# 2.1 Install rgee Python dependencies - OPTION 01
 # --------------------------------------------------------------
 library(rgee)
-ee_Initialize()
+ee_install() # Window user must need to terminate R and enter to R again.
+
 
 # --------------------------------------------------------------
-# 5. Helper function
+# 2.2 Load a Python ENV with the rgee requirements installed - OPTION 02
 # --------------------------------------------------------------
-ee_help(ee$Algorithms$CannyEdgeDetector)
+# IMPORTANT: Change 'py_path' argument for your own Python PATH
+ee_install_set_pyenv(
+  py_path = "/home/pc-user01/.virtualenvs/rgee/bin/python",
+  py_env = "rgee" # Change it for your own Python ENV
+) 
+# Window user must need to terminate R and enter to R again.
+ee_check()
+
+# --------------------------------------------------------------
+# 3. Check out whether your Python and R requirements are properly set up.
+# --------------------------------------------------------------
+ee_check()
+
+# --------------------------------------------------------------
+# 4. Authenticate and Initialize Earth Engine
+# --------------------------------------------------------------
+
+# Initialize just Earth Engine
+ee_Initialize()
+
+# Initialize Earth Engine and GD
+ee_Initialize(drive = TRUE)
+
+# Initialize Earth Engine and GCS
+ee_Initialize(gcs = TRUE)
+
+# Initialize Earth Engine, GD and GCS
+ee_Initialize(drive = TRUE, gcs = TRUE)
